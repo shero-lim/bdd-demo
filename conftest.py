754dd5201e1ae1fd26a1574290cfe647ebaae96d
shero.lin <2390378545@qq.com>
2022-12-19 18:12:23 +0800

    fix

--- conftest.py
+++ conftest.py
@@ -7,15 +7,21 @@ from selenium import webdriver
 
 
 def pytest_bdd_before_scenario(request, feature, scenario):
-    print(request, feature, scenario)
+    pass
 
 
 def pytest_bdd_after_scenario(request, feature, scenario):
     pass
 
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 def browser(request):
-    pm = WebdriverManager().get_browser_page_model(PageModel, config["default_browser"], config["selenium_server"])
-    # driver = webdriver.Remote(config["selenium_server"], desired_capabilities={"browserName": "chrome"})
+    wm = WebdriverManager()
+    pm = wm.get_browser_page_model(PageModel, config["default_browser"], config["selenium_server"])
+
+    def fn():
+        print("当用例执行完之后：teardown driver!")
+        wm.teardown()
+
+    request.addfinalizer(fn)
     return pm
